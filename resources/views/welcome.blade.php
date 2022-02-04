2ad7fb4cc4aec851ba484af65ff452ee1b4e7f92
Jagdeep Singh <jdecode@gmail.com>
2022-02-04 10:03:05 +0530

    Replace CDN with CSS asset file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">

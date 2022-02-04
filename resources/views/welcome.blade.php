d2a9f02706fcf70f839af95c427ff080c0864000
Jagdeep Singh <jdecode@gmail.com>
2022-02-04 19:27:23 +0530

    Custom changes (prior to Breeze)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,190 +6,47 @@
 
         <title>{{ config('app.name', 'Laravel')  }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <body class="antialiased bg-black">
+        <div class="relative flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900 py-4 sm:pt-0">
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <span class="mx-auto">
-                    Links!
-                </span>
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-                        <div class="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 ">
+                <div class="mt-8 dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <!-- This example requires Tailwind CSS v2.0+ -->
+                    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                        <li class="col-span-1 rounded-lg shadow divide-y divide-gray-200">
+                            <div class="w-full flex items-center justify-between p-6 space-x-6 bg-purple-800">
+                                <div class="flex-1 truncate">
+                                    <div class="flex items-center space-x-3">
+                                        <h3 class="text-purple-900 text-sm font-medium truncate">Heroku</h3>
+                                        <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">DevOps</span>
+                                    </div>
+                                    <p class="mt-1 text-gray-500 text-sm truncate">PHP Node.js React</p>
+                                </div>
+                                <img class="w-10" src="{{ asset('heroku-logo.png')  }}" alt="Heroku">
+                            </div>
                             <div>
-                              <span class="rounded-lg inline-flex p-3 bg-teal-50 text-teal-700 ring-4 ring-white h-24 w-24">
-                                  <img src="{{ asset('heroku-logo.png') }}" alt="Heroku" />
-                              </span>
+                                <div class="-mt-px flex divide-x divide-gray-200">
+                                    <div class="w-0 flex-1 flex">
+                                        <a target="_blank" href="https://dashboard.heroku.com" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                            <button class="ml-3">Open</button>
+                                        </a>
+                                    </div>
+                                    <div class="-ml-px w-0 flex-1 flex">
+                                        <a href="tel:+1-202-555-0170" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                                            <!-- Heroicon name: solid/phone -->
+                                            <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                            </svg>
+                                            <span class="ml-3">Call</span>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="https://dashboard.heroku.com" target="_blank" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Heroku
-                                    </a>
-                                </h3>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
+                        </li>
+                    </ul>
 
-                        <div class="sm:rounded-tr-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                            <div>
-                              <span class="rounded-lg inline-flex p-3 bg-purple-50 text-purple-700 ring-4 ring-white">
-                                <!-- Heroicon name: outline/badge-check -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="#" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Benefits
-                                    </a>
-                                </h3>
-                                <p class="mt-2 text-sm text-gray-500">
-                                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                                </p>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
-
-                        <div class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                            <div>
-                              <span class="rounded-lg inline-flex p-3 bg-sky-50 text-sky-700 ring-4 ring-white">
-                                <!-- Heroicon name: outline/users -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="#" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Schedule a one-on-one
-                                    </a>
-                                </h3>
-                                <p class="mt-2 text-sm text-gray-500">
-                                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                                </p>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
-
-                        <div class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                            <div>
-                              <span class="rounded-lg inline-flex p-3 bg-yellow-50 text-yellow-700 ring-4 ring-white">
-                                <!-- Heroicon name: outline/cash -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="#" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Payroll
-                                    </a>
-                                </h3>
-                                <p class="mt-2 text-sm text-gray-500">
-                                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                                </p>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
-
-                        <div class="sm:rounded-bl-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                            <div>
-                              <span class="rounded-lg inline-flex p-3 bg-rose-50 text-rose-700 ring-4 ring-white">
-                                <!-- Heroicon name: outline/receipt-refund -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z" />
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="#" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Submit an expense
-                                    </a>
-                                </h3>
-                                <p class="mt-2 text-sm text-gray-500">
-                                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                                </p>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
-
-                        <div class="rounded-bl-lg rounded-br-lg sm:rounded-bl-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                            <div>
-                              <span class="rounded-lg inline-flex p-3 bg-indigo-50 text-indigo-700 ring-4 ring-white">
-                                <!-- Heroicon name: outline/academic-cap -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-                                </svg>
-                              </span>
-                            </div>
-                            <div class="mt-8">
-                                <h3 class="text-lg font-medium">
-                                    <a href="#" class="focus:outline-none">
-                                        <!-- Extend touch target to entire panel -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        Training
-                                    </a>
-                                </h3>
-                                <p class="mt-2 text-sm text-gray-500">
-                                    Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.
-                                </p>
-                            </div>
-                            <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                              </svg>
-                            </span>
-                        </div>
-                    </div>
 
                 </div>
 

f844fd564f52b7236f2b3e9cc93f06d8f8ea0029
Jagdeep Singh <jdecode@gmail.com>
2022-02-06 22:03:00 +0530

    Dark mode persists (local storage), button to toggle, and keyboard shortcut as well (Shift + D)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,9 +8,19 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <script>
-            if (localStorage.dark !== 'undefined' && localStorage.dark && localStorage.dark === 'true') {
-                document.documentElement.classList.add('dark');
-                localStorage.dark = true;
+            function toggleDark() {
+                document.documentElement.classList.toggle('dark');
+                localStorage.dark = !JSON.parse(localStorage.dark);
+            }
+
+            if (localStorage.dark !== 'undefined') {
+                if(localStorage.dark === 'true') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.dark = true;
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.dark = false;
+                }
             }
         </script>
 
@@ -21,11 +31,26 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-gray-200 dark:bg-gray-900 container mx-auto">
+        <div class="fixed top-0 right-0 h-6 w-14 mt-1 mr-2">
+            <button class="focus:outline-none p-1 border border-dashed border-gray-500" onclick="toggleDark()">
+                <i class="flex min-w-6">
+                    <img src="{{ asset('/images/moon-black.svg') }}" alt="moon" class="h-6 w-6">
+                    <img src="{{ asset('/images/sun-color.svg') }}" alt="sun" class="h-6 w-6">
+                </i>
+            </button>
+        </div>
         @inertia
 
         @env ('local')
             <script src="http://localhost:8080/js/bundle.js"></script>
         @endenv
+        <script>
+            document.onkeydown = function(e) {
+                if (e.shiftKey && e.which === 68) {
+                    toggleDark();
+                }
+            };
+        </script>
     </body>
 </html>
